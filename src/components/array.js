1f4f7fd6e9a729925dc392206be87cef6ffbe7af
Adewale Oluwatoyin <adewaleoluwatoyin81@gmail.com>
2023-02-17 23:58:56 +0100

    Card Details

---
+++ src/components/array.js
@@ -0,0 +1,76 @@
+const friends =  [
+    {
+        id: 1,
+        name: 'Franc Enemuo',
+        interest: ['Football', 'Tennis', 'Politics'],
+        profession: 'Software Developer',
+        phoneNumber:'08100000000',
+        stateOfOrigin: 'Anambra'    
+        }, 
+        {
+        id: 2,
+        name: 'Ola Debo',
+        interest: ['Swimming', 'Reading', 'Boxing'],
+        profession: 'Doctor',
+        phoneNumber:'08123456789',
+        stateOfOrigin: 'Delta'    
+        }, 
+        {
+        id: 3,
+        name: 'Haruna Lukman',
+        interest: ['Singing', 'Soccer', 'Dancing'],
+        profession: 'Caterer',
+        phoneNumber:'08189345890',
+        stateOfOrigin: 'Kaduna'    
+        },
+        {
+        id: 4,
+        name: 'Ugochi Ameh',
+        interest: ['Dancing', 'Cooking', 'Football'],
+        profession: 'Data Analyst',
+        phoneNumber:'08122225890',
+        stateOfOrigin: 'Edo'    
+        },
+        {
+        id: 5,
+        name: 'Effiong Epeh',
+        interest: ['Cooking', 'Swimming', 'Music'],
+        profession: 'Nurse',
+        phoneNumber:'08189343450',
+        stateOfOrigin: 'Delta'    
+        },
+        {
+        id: 6,
+        name: 'Ezia Amadi',
+        interest: ['History', 'Swimming', 'Dancing'],
+        profession: 'Writer',
+        phoneNumber:'08170043450',
+        stateOfOrigin: 'Kano'    
+        },
+        {
+        id: 7,
+        name: 'Akadiri Dami',
+        interest: ['History', 'Music', 'Dancing'],
+        profession: 'Backened Developer',
+        phoneNumber:'08154903450',
+        stateOfOrigin: 'Ondo'    
+         },
+        {
+        id: 8,
+        name: 'Sheriff Sheriff',
+        interest: ['Writing', 'Cooking', 'Dancing'],
+        profession: 'Data science',
+        phoneNumber:'08154903643',
+        stateOfOrigin: 'Kwara'    
+        },
+        {
+        id: 9,
+        name: 'Wales Wales',
+        interest: ['History', 'Swimming', 'Dancing'],
+        profession: 'UI/UX developer',
+        phoneNumber:'08154902323',
+        stateOfOrigin: 'Osun'  
+
+        }
+
+]
